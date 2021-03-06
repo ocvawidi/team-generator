6ac3791e5de0af51f34dd5b182bce924c2e3713b
Ryu Kurnianto <ryuuwiz@gmail.com>
2021-03-06 10:40:41 +0700

    Add HTML Structure, Basic CSS, and DOM Selectors

---
+++ app.js
@@ -0,0 +1,7 @@
+// Selectors
+const groupInput = document.querySelector("#groupInput");
+const nameInput = document.querySelector("#nameInput");
+const SubmitButton = document.querySelector(".SubmitButton");
+const Group = document.querySelector(".Group");
+
+// Functions
