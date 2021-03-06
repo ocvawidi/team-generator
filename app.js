279bb727843474b0417984961a69ca25cd10ca66
Ryu Kurnianto <ryuuwiz@gmail.com>
2021-03-06 13:36:00 +0700

    Add new Function

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 // Selectors
-const groupInput = document.querySelector("#groupInput");
-const nameInput = document.querySelector("#nameInput");
-const SubmitButton = document.querySelector(".SubmitButton");
-const Group = document.querySelector(".Group");
+const groupInput = document.getElementById("groupInput");
+const nameInput = document.getElementById("nameInput");
+const SubmitButton = document.getElementById("SubmitButton");
+const Group = document.getElementById("Group");
 
-// Functions
+SubmitButton.addEventListener('click', e => {
+    e.preventDefault();
+    RandomizeTeam();
+});
+
+function RandomizeTeam() {
+    const allNames = nameInput.value.split("/n");
+    const namesperGroup = parseInt(groupInput.value);
+}
